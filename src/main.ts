9cb8e2bf69739168f8751705b8445ed3d06a66d9
Abijithbabu <abijithb2002@gmail.com>
2023-11-22 11:34:04 +0530

    validation added

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  // Example CORS configuration
-app.use((req, res, next) => {
-  res.header('Content-Range', 'tasks 0-20/20')
-  next()
-});
+  app.enableCors({
+    exposedHeaders: ['Content-Range'],
+  })
+  app.useGlobalPipes(new ValidationPipe());
+  app.use((req, res, next) => {
+    res.header('Content-Range', 'tasks 0-1/2');
+    next();
+  });
 
   await app.listen(5000);
 }
